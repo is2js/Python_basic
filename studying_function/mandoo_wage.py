1ea9f278dc5824fb908f85d8e421698ecc062327
is2js_notebook <tingstyle1@gmail.com>
2018-07-03 23:29:08 +0900

    6 . 함수(function) 학습하기 (구구단출력 / for문 속 함수 / default인자)

--- studying_function/mandoo_wage.py
+++ studying_function/mandoo_wage.py
@@ -1,11 +1,11 @@
 
-def wage_calculator(work_start, work_finish):
+def wage_calculator(work_start, work_finish, wage_per_hour=7530):
     worked_hours = work_finish - work_start
     #노동법에 의해, 근무시간 조정
     if worked_hours >= 4:
         break_time = ( worked_hours // 4 ) * 0.5
         worked_hours = worked_hours - break_time
-    wage = worked_hours * 7530
+    wage = worked_hours * wage_per_hour
     return wage, worked_hours
 
 print('만두가게 임금계산하는 프로그램')
@@ -19,7 +19,7 @@ work_start_list = [ 10, 9, 8, 9, 8 ]
 work_finish_list = [ 13, 18, 17, 13, 14 ]
 
 for i in range(0, len(employee_list)):
-    wage_of_the_day, worked_hours = wage_calculator(work_start_list[i],  work_finish_list[i])
+    wage_of_the_day, worked_hours = wage_calculator(work_start_list[i],  work_finish_list[i], 8000)
     print('{}님 : {}시간 근무 :: 일당은 {}원'.format(
         employee_list[i], worked_hours, wage_of_the_day
     ))
