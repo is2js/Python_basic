6a709ef5c3acd2760f9fb3b2f93a1cf612dd01f2
is2js_notebook <tingstyle1@gmail.com>
2018-07-04 13:53:58 +0900

    7. 파일 open시 주의사항 주석달기

--- studying_function/write_txt_file.py
+++ studying_function/write_txt_file.py
@@ -1,3 +1,4 @@
+#txt는 utf-8인코딩을 직접 해줘야 한글이 안깨진다.
 f = open( 'first.txt', 'w', encoding='utf-8')
 f.write('hello world\n')
 f.write('hello File\n')
@@ -5,7 +6,8 @@ f.write('한글도 출력하고 싶어요.')
 f.close()
 
 #csv : comma separated values
-csv_file = open('csv_test_file.csv', 'w', encoding='utf-8')
+#csv는 utf-8인코딩시에 엑셀에서 깨져버린다.
+csv_file = open('csv_test_file.csv', 'w')
 csv_file.write('1, 3, 5\n')
 csv_file.write('7, 9, 11')
 csv_file.close()
