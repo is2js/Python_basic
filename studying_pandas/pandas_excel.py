30e45ccab3795ce6318c1d52a071c34df1346aed
is2js_notebook <tingstyle1@gmail.com>
2018-07-11 14:46:16 +0900

    16. pandas를 이용해 차트그리기

--- studying_pandas/pandas_excel.py
+++ studying_pandas/pandas_excel.py
@@ -1,6 +1,13 @@
 import pandas as pd
-
+import matplotlib
 import matplotlib.pyplot as plt
+
+from matplotlib import font_manager
+
+#matplotlib의 폰트를 지정해주는 방법
+font_name = font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
+matplotlib.rc('font', family=font_name)
+
 
 #엑셀파일 열기 - xlrd 모듈 필요
 df = pd.read_excel('mandoo_management.xlsx', sheet_name='Sheet1')
@@ -13,11 +20,18 @@ df['시간당 만두'] = df['만두생산'] / df['근무시간']
 df = df.sort_values(by=['시간당 만두', '근무시간'], ascending=[False, False])
 
 #최종 DataFrame 확인
-print(df)
-
+# print(df)
+#DF의 이름열-> INDEX로 옮긴다.
+df = df.set_index('이름')
+#print(df)
 #플롯 그리기
 df['만두생산'].plot(kind='bar')
+print(df['만두생산'])
+#플롯 저장하기는 반드시 plt.show로 띄우기전에 먼저 저장하기!
+plt.savefig('만두생산.png')
+#플롯 띄우기
 plt.show(block=True)
+
 
 
 
